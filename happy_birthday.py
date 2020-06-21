111f7e4c0dfb88da1bd2f6fbfbf6c5242e8e2180
brady <brady-liang123@icloud.com>
2020-06-21 10:25:28 -0700

    changing name to full name

--- happy_birthday.py
+++ happy_birthday.py
@@ -1,4 +1,4 @@
 # Happy Birthday.py
-your_name = input("What is your name?")
+your_name = input("What is your full name?")
 your_birthday = input("When is your birthday?")
 print("Today is " + your_birthday + "." + "Happy Birthday," + your_name + "!")
