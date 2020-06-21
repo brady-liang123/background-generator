d04d2fc03ae6b6c09a17ae0c14e24dd708f29ecc
Cecilia Jiang <xiaoayuanj@yahoo.com>
2020-06-21 10:35:18 -0700

    reverting back

--- happy_birthday.py
+++ happy_birthday.py
@@ -1,4 +1,4 @@
 # Happy Birthday.py
-your_name = input("What is your full name?")
+your_name = input("What is your first name?")
 your_birthday = input("When is your birthday?")
 print("Today is " + your_birthday + "." + "Happy Birthday," + your_name + "!")
